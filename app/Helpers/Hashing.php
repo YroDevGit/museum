2f68bc855d796a4a0adbd5afe58d39b0d9d69e6f
Tyrone <tyrone@clearmindai.com>
2025-08-25 00:05:12 +0800

    asdfg

--- app/Helpers/Hashing.php
+++ app/Helpers/Hashing.php
@@ -1,7 +1,7 @@
 <?php
 if(! function_exists("my_hash")){
     function my_hash(String $text, $length = 16){
-        return substr(md5($text), 0, $length);
+     return substr(md5($text), 0, $length);
     }
 }
 ?>
