4bbb7b767ceeb4df3b3215228328af2503de5263
Tyrone <tyrone@clearmindai.com>
2025-08-25 07:24:38 +0800

    dad

--- DATABASE.sql
+++ DATABASE.sql
@@ -19,6 +19,18 @@
 CREATE DATABASE IF NOT EXISTS `album` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `album`;
 
+-- Dumping structure for table album.admin
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table album.admin: ~1 rows (approximately)
+REPLACE INTO `admin` (`id`, `username`, `password`) VALUES
+	(1, 'admin', 'admin');
+
 -- Dumping structure for table album.album
 CREATE TABLE IF NOT EXISTS `album` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -28,10 +40,13 @@ CREATE TABLE IF NOT EXISTS `album` (
   `date_upd` datetime DEFAULT NULL ON UPDATE current_timestamp(),
   `status` enum('live','longterm') NOT NULL DEFAULT 'live',
   `venue_id` int(11) NOT NULL,
+  `log` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table album.album: ~0 rows (approximately)
+REPLACE INTO `album` (`id`, `remote_id`, `date_add`, `date_over`, `date_upd`, `status`, `venue_id`, `log`) VALUES
+	(91, 117, '2025-08-24 11:52:39', '2025-08-24 11:52:39', '2025-08-24 19:53:06', 'longterm', 1, 1);
 
 -- Dumping structure for table album.authentications
 CREATE TABLE IF NOT EXISTS `authentications` (
@@ -91,9 +106,30 @@ CREATE TABLE IF NOT EXISTS `cookies` (
   `status` int(11) DEFAULT 1,
   `token` text DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table album.cookies: ~0 rows (approximately)
+-- Dumping data for table album.cookies: ~17 rows (approximately)
+REPLACE INTO `cookies` (`id`, `album`, `user`, `remote_id`, `remotetoken`, `date_created`, `status`, `token`) VALUES
+	(15, 73, 132, 117, '31bb2073555a73b5', '2025-08-24 10:41:01', 1, '774a1e2dc984563c'),
+	(16, 74, 133, 117, '31bb2073555a73b5', '2025-08-24 10:45:34', 1, 'af900f0100f25e61'),
+	(17, 76, 135, 117, '31bb2073555a73b5', '2025-08-24 10:50:52', 1, 'b6f66c0f32e6bdc5'),
+	(18, 77, 136, 117, '31bb2073555a73b5', '2025-08-24 10:56:13', 1, '3276c923362bfd0f'),
+	(19, 78, 137, 117, '31bb2073555a73b5', '2025-08-24 10:58:06', 1, '9967c8c4d48ba949'),
+	(20, 80, 139, 117, '31bb2073555a73b5', '2025-08-24 11:01:36', 1, '073b51468632a153'),
+	(21, 81, 140, 117, '31bb2073555a73b5', '2025-08-24 11:03:04', 1, '65fbd82718e27fc8'),
+	(22, 82, 141, 117, '31bb2073555a73b5', '2025-08-24 11:14:42', 1, '1e874a5b2c27e517'),
+	(23, 85, 144, 117, '31bb2073555a73b5', '2025-08-24 11:17:36', 1, 'ff756dbd87a7fe2e'),
+	(24, 86, 145, 117, '31bb2073555a73b5', '2025-08-24 11:21:20', 1, '60d028d75ad3a59f'),
+	(25, 87, 146, 117, '31bb2073555a73b5', '2025-08-24 11:23:03', 1, '29c147baff07aca1'),
+	(26, 88, 147, 117, '31bb2073555a73b5', '2025-08-24 11:30:14', 1, '7ebfd534647ae348'),
+	(27, 88, 147, 117, '31bb2073555a73b5', '2025-08-24 11:51:55', 1, '7ebfd534647ae348'),
+	(28, 91, 150, 117, '31bb2073555a73b5', '2025-08-24 11:53:06', 1, 'e30d6f8551c8b036'),
+	(29, 91, 150, 117, '31bb2073555a73b5', '2025-08-24 11:53:33', 1, 'e30d6f8551c8b036'),
+	(30, 91, 150, 117, '31bb2073555a73b5', '2025-08-24 11:53:51', 1, 'e30d6f8551c8b036'),
+	(31, 91, 150, 117, '31bb2073555a73b5', '2025-08-24 11:54:50', 1, 'e30d6f8551c8b036'),
+	(32, 91, 150, 117, '31bb2073555a73b5', '2025-08-24 11:57:20', 1, 'e30d6f8551c8b036'),
+	(33, 91, 150, 117, '31bb2073555a73b5', '2025-08-24 11:57:35', 1, 'e30d6f8551c8b036'),
+	(34, 91, 150, 117, '31bb2073555a73b5', '2025-08-24 11:59:54', 1, 'e30d6f8551c8b036');
 
 -- Dumping structure for table album.devices
 CREATE TABLE IF NOT EXISTS `devices` (
@@ -132,16 +168,17 @@ CREATE TABLE IF NOT EXISTS `invite_token` (
   `status` int(11) DEFAULT 0,
   `token` text DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table album.invite_token: ~2 rows (approximately)
+-- Dumping data for table album.invite_token: ~7 rows (approximately)
 REPLACE INTO `invite_token` (`id`, `remote_id`, `album_id`, `remote_token`, `date_added`, `status`, `token`) VALUES
 	(5, 119, 38, '5fb60d75ce6763d7', '2025-07-30 06:42:01', 0, '0a23f727e9ed8c82'),
 	(6, 119, 38, '5fb60d75ce6763d7', '2025-07-30 06:49:06', 0, '0a23f727e9ed8c82'),
 	(7, 119, 39, '5fb60d75ce6763d7', '2025-07-30 08:21:41', 0, '0a23f727e9ed8c82'),
 	(8, 119, 42, '5fb60d75ce6763d7', '2025-08-01 10:30:32', 0, '0a23f727e9ed8c82'),
 	(9, 119, 70, '5fb60d75ce6763d7', '2025-08-04 12:53:26', 0, '34f05f39c1394d39'),
-	(10, 119, 70, '5fb60d75ce6763d7', '2025-08-04 13:02:28', 0, '34f05f39c1394d39');
+	(10, 119, 70, '5fb60d75ce6763d7', '2025-08-04 13:02:28', 0, '34f05f39c1394d39'),
+	(11, 119, 71, '5fb60d75ce6763d7', '2025-08-04 13:44:24', 0, '34f05f39c1394d39');
 
 -- Dumping structure for table album.jobs
 CREATE TABLE IF NOT EXISTS `jobs` (
@@ -217,18 +254,23 @@ CREATE TABLE IF NOT EXISTS `photobooth` (
 CREATE TABLE IF NOT EXISTS `remote` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `venue_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
   KEY `venue_id` (`venue_id`),
   CONSTRAINT `remote_ibfk_1` FOREIGN KEY (`venue_id`) REFERENCES `venue` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=278 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=344 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table album.remote: ~5 rows (approximately)
-REPLACE INTO `remote` (`id`, `venue_id`) VALUES
-	(117, 1),
-	(118, 1),
-	(119, 1),
-	(120, 1),
-	(277, 2);
+-- Dumping data for table album.remote: ~9 rows (approximately)
+REPLACE INTO `remote` (`id`, `venue_id`, `status`) VALUES
+	(117, 1, 1),
+	(118, 1, 1),
+	(119, 1, 1),
+	(120, 1, 1),
+	(122, 1, 1),
+	(150, 1, 0),
+	(153, 1, 0),
+	(158, 1, 0),
+	(277, 2, 0);
 
 -- Dumping structure for table album.sessions
 CREATE TABLE IF NOT EXISTS `sessions` (
@@ -243,15 +285,11 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   KEY `sessions_last_activity_index` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table album.sessions: ~6 rows (approximately)
+-- Dumping data for table album.sessions: ~3 rows (approximately)
 REPLACE INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-	('4ds47pX0uZNsWjGUeuaRAo4NzMMKkjqqcdYRumch', NULL, '127.0.0.1', 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Mobile Safari/537.36 Edg/138.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoialkyY1I0SlZySU5FQmpQSEZZYmkzSlE0NUZpT0tlb0FuQm1kYkJSdCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NjA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9waG90b2dyYXBoZXIvYWxidW0vNzAvdXNlci8xMDIvaWNvLnBuZyI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1754312494),
-	('c2eQZUpw5YISlplkxzT3CWvIeCm0DPvYtB2ep4Jw', NULL, '127.0.0.1', 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Mobile Safari/537.36 Edg/138.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiQlVGcFZRcGExdkR0SlBDNUVJY25lU0VJREl1NWpPVk9xZFpYN002MCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1754312973),
-	('D87rEPNiHfTaw9e6zrdX7IFYQyq9lrFmeKhdFknS', NULL, '127.0.0.1', 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Mobile Safari/537.36 Edg/138.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoicjNpMzU2S3VJZWc5aTAyZUpORUpkdW9mbkNxQ0FBYUpWbXI3a1BTWiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1754313145),
-	('L4qqdSLKyIUW6fFyrDDGHzMazNvqoYpaG1Dn4fDN', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YToyOntzOjY6Il90b2tlbiI7czo0MDoiYW9OMld1V2RzNmhhVTI4ODRUQW1HQnhYeHJhTkhydkdMcDZ2NEFsTiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1754313161),
-	('mkTDbTPvv40iYehRWLavod5mugVif4ok2NFGLONf', NULL, '127.0.0.1', 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Mobile Safari/537.36 Edg/138.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiQTRDZ1A2WlBaSkFJamJxZE5YTk9WMWdreGl4UnRQN0NxTExhdVpTWSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjk6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYWQuanBnIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1754312552),
-	('t61eylpmvajzVDrWy50DcElOuAxvQHhhdzj5FbUm', NULL, '127.0.0.1', 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Mobile Safari/537.36 Edg/138.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiQ29tek83N05sTmtMR2txWGJBZGNkMFl5dnNDaXI3Z0hCZ2N1VlNSVCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NjA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9waG90b2dyYXBoZXIvYWxidW0vNzAvdXNlci8xMDQvaWNvLnBuZyI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1754312662),
-	('ZGobc4n0bSkJ4djxBAyixUm5Ll8JhyVyItWLQHaG', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoickoyOEZsclV0aWVNcXZOOEw1MExIRzNjMmE5WlhUSG5oNDZxMnlITCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NTU6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9waG90b2dyYXBoZXIvYWxidW0vMjAvdXNlci8zNy8yMDAiO319', 1754313157);
+	('3qdiE10C1brsBZtI9UZBYQpXleqRJ5UT1HQ2v6ZG', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiZVYzMVJCZkJ4Nm9oS05TR2NXRWRZZ2NrSE40c0I1R1RaU3pQRWludSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjk6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYWQuanBnIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1756037079),
+	('Nijg6Mk5R3NWSklzVIKXsKBGRfhncGm3ov9HZjiv', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiOUFlbUd3SUNXeWVEaFNZVkdBTXM4RmNVcGs5akFYT3FpUXlseE9IdiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1756037095),
+	('OzfpOnMbBkH1DTqaf7iZuusy9A3OeYr3Z0v9IkoA', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiZm5uNkJTaHZmQjE1Q2x4RG9jTlhKeXFuNTJwQldSeTBMUlh5SGtjOCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjk6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYWQuanBnIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1756036801);
 
 -- Dumping structure for table album.user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -264,9 +302,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `org` int(11) DEFAULT 1,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=105 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=151 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table album.user: ~0 rows (approximately)
+-- Dumping data for table album.user: ~1 rows (approximately)
+REPLACE INTO `user` (`id`, `date_add`, `album_id`, `email`, `name`, `log`, `org`) VALUES
+	(150, '2025-08-24 11:52:39', 91, 'tyronemalocon@gmail.com', 'Tyrone Malocon Malocon', '', 1);
 
 -- Dumping structure for table album.users
 CREATE TABLE IF NOT EXISTS `users` (
