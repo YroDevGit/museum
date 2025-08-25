401361c9920e1efd3ff2ffc05e6e45fd0f2ebb61
Tyrone <tyrone@clearmindai.com>
2025-08-25 09:54:49 +0800

    dasda

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,8 +8,16 @@ abstract class Controller
 
     public function helper(string $helper)
     {
-        require_once base_path("app/Helpers/$helper.php");
+        // __DIR__ points to the directory of the current PHP file
+        $path = __DIR__ . '/../Helpers/' . $helper . '.php';
+
+        if (file_exists($path)) {
+            require_once $path;
+        } else {
+            throw new \Exception("Helper file not found: $path");
+        }
     }
+
 
     public function services(string $services)
     {
